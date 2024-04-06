06262f9f1158ef49862db4b8d080354cc00058d8
Anton Vasyliev <anton12vasyliev@gmail.com>
2024-04-06 13:42:57 +0100

    fix/login

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,16 +3,14 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user-model");
 const gravatar = require("gravatar");
 const sendEmail = require("../service/mail-service");
-// const mailService = require("../service/mail-service");
+
 const tokenService = require("../service/token-service");
-// const crypto = require("node:crypto");
+
 const uuid = require("uuid");
 const UserDto = require("../dtos/user-dtos");
 
 const Joi = require("joi");
 const userModel = require("../models/user-model");
-// const { UUID } = require("mongodb");
-// const { token } = require("morgan");
 
 const userSchema = Joi.object({
   name: Joi.string().min(3).max(30),
@@ -44,7 +42,6 @@ async function register(req, res, next) {
       avatarURL,
       activationLink,
     });
-    // text: `To confirm your registration please click on the <a href="https://back-vinyl-records.onrender.com/auth/verify/">link</a>`,
 
     await sendEmail({
       to: email,
@@ -58,7 +55,7 @@ async function register(req, res, next) {
         <p>Remember, our community is all about sharing your favorite vinyl records and finding new music!</p>
         <p>We're excited to have you join us!</p>
         <p>Best,<br/>The VinylRecords Team</p>
-        <a href="https://back-vinyl-records.onrender.com/auth/activate${activationLink}" style="display: inline-block; background-color: #007bff; color: #ffffff; text-decoration: none; padding: 10px 20px; border-radius: 5px; font-weight: bold;">Confirm Registration</a>
+        <a href="http://localhost:3000/auth/activate/${activationLink}" style="display: inline-block; background-color: #007bff; color: #ffffff; text-decoration: none; padding: 10px 20px; border-radius: 5px; font-weight: bold;">Confirm Registration</a>
       </div>
     </div>
       `,
@@ -94,7 +91,7 @@ async function login(req, res, next) {
 
   try {
     let user = await User.findOne({ email });
-    await User.findByIdAndUpdate(user._id, { isLoggedIn: true });
+
     if (!user) {
       return res.status(401).send({ message: "User is not found" });
     }
@@ -107,6 +104,8 @@ async function login(req, res, next) {
     if (!user.isActivated) {
       return res.status(401).send({ message: "Your account is not verified" });
     }
+
+    await User.findByIdAndUpdate(user._id, { isLoggedIn: true });
 
     const userDto = new UserDto(user);
     const tokens = tokenService.generateTokens({ ...userDto });
@@ -131,54 +130,37 @@ async function logout(req, res, next) {
     const { refreshToken } = req.cookies;
     const token = await tokenService.removeToken(refreshToken);
     let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
     await User.findByIdAndUpdate(user._id, { isLoggedIn: false });
     res.clearCookie("refreshToken");
-    return res.json(token);
+    return res.status(200).send({ message: "Logout successful" });
   } catch (error) {
     next(error);
   }
 }
 
-// async function resendVerify(req, res, next) {
-//   const { email } = req.body;
-
-//   try {
-//     const user = await User.findOne({ email });
-//     if (!user) {
-//       return res.status(404).send({ message: "User not found" });
-//     }
-//     if (user.verify) {
-//       return res
-//         .status(400)
-//         .send({ message: "Verification has already been passed" });
-//     }
-//     await sendEmail({
-//       to: "anton12vasyliev@gmail.com",
-//       from: "anton12vasyliev@gmail.com",
-//       subject: "Hello bulo4ka",
-//       html: `To confirm your registration please click on the <a href="https://back-vinyl-records.onrender.com/auth/verify/${verifyToken}">link</a>`,
-//       text: `To confirm your registration please click on the <a href="https://back-vinyl-records.onrender.com/auth/verify/${verifyToken}">link</a>`,
-//     });
-//     return res.status(200).send({ message: "Verification email sent" });
-//   } catch (error) {
-//     next(error);
-//   }
-// }
-
 async function refresh(req, res, next) {
   try {
     const { refreshToken } = req.cookies;
     if (!refreshToken) {
-      res.status(404).send({ message: "User not authorized 1" });
+      return res.status(401).send({ message: "Refresh token not provided" });
     }
     const userData = tokenService.validateRefreshToken(refreshToken);
     const tokenFromDb = await tokenService.findToken(refreshToken);
-    // const tokenFromDb = await tokenModel.findOne({ refreshToken });
     if (!userData || !tokenFromDb) {
-      console.log(tokenFromDb);
-      res.status(404).send({ message: "User not authorized 2" });
+      return res
+        .status(401)
+        .send({ message: "Invalid or expired refresh token" });
     }
     const user = await userModel.findById(userData.id);
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+    if (!user.isActivated) {
+      return res.status(401).send({ message: "User account is not activated" });
+    }
     const userDto = new UserDto(user);
     const tokens = tokenService.generateTokens({ ...userDto });
     await tokenService.saveToken(userDto.id, tokens.refreshToken);
@@ -186,11 +168,13 @@ async function refresh(req, res, next) {
       maxAge: 30 * 24 * 60 * 60 * 1000,
       httpOnly: true,
     });
-    return res.status(201).send({
+    return res.status(200).send({
       user: userDto,
       ...tokens,
     });
-  } catch (error) {}
+  } catch (error) {
+    next(error);
+  }
 }
 
 async function activate(req, res, next) {
@@ -198,23 +182,22 @@ async function activate(req, res, next) {
     const activationLink = req.params.link;
     const user = await User.findOne({ activationLink });
     if (!user) {
-      console.log("No user found with the activation link:", activationLink);
-      return;
+      return res
+        .status(404)
+        .send({ message: "User not found with the activation link provided" });
     }
-
     user.isActivated = true;
     await user.save();
     res.redirect(process.env.CLIENT_URL);
-  } catch (error) {}
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = {
   register,
   login,
   logout,
-  // current,
-  // verify,
-  // resendVerify,
   refresh,
   activate,
 };
